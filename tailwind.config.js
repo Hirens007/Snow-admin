3558566c7d864bb10953904bafadaf371d236101
Hirens007 <125988452+Hirens007@users.noreply.github.com>
2023-02-27 15:27:06 +0530

    setup admin panal

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        black: {
+          DEFAULT: "#1c1c1c",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
