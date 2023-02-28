ec6ce7540e5506dfe46063961333c8a84169cd6f
Hirens007 <125988452+Hirens007@users.noreply.github.com>
2023-02-28 19:33:00 +0530

    updated admin

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,5 +21,9 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "base", // only generate global styles
+    }),
+  ],
 }
